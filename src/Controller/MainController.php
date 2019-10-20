e0dc67a436796a69a34cf527a79dd4cf1c249b60
Juan Leon <juansleonc@gmail.com>
2019-10-19 19:11:13 -0500

    Init database
    Create Category and products

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     */
+    public function  custom (Request $request)
+    {
+        dump($request);
+        return new Response('<h1>Welcome {{ name }}</h1>');
+    }
+}
